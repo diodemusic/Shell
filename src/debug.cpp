f05918071de1402f08376ef823052674a6e53404
saves <diodemusicuk@gmail.com>
2025-03-03 16:55:21 +0000

    Overload debug statement

--- src/debug.cpp
+++ src/debug.cpp
@@ -12,3 +12,12 @@ void debug(std::string str)
         std::cout << "DEBUG: " << str << "\n";
     }
 }
+
+// * overload not needed, we arent printing any ints, uncomment if needed in the future
+// void debug(int num)
+// {
+//     if (DEBUG)
+//     {
+//         std::cout << "DEBUG: " << num << "\n";
+//     }
+// }
