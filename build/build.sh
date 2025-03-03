b5b8e53bb58f50a2d85d4c1e36f090c153182415
saves <diodemusicuk@gmail.com>
2025-03-03 17:01:28 +0000

    Move build files

---
+++ build/build.sh
@@ -0,0 +1,2 @@
+echo "Compiling..."
+g++ -o shell.out --std=c++20 ../src/main.cpp ../src/shell.cpp ../src/debug.cpp ../src/echo.cpp
--- src/build.sh
+++
@@ -1,4 +0,0 @@
-echo "Compiling..."
-g++ -o shell.out --std=c++20 main.cpp shell.cpp debug.cpp echo.cpp
-echo "Running..."
-./shell.out
